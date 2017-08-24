3954f742c789c2fd46b5dd47b4d4491ef2c28499
Matthew Peterson <matt2694@edu.eal.dk>
2017-08-24 13:06:09 +0200

    jk, Exercises 1-3

--- 24_8_17_Server/Program.cs
+++ 24_8_17_Server/Program.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Net.Sockets;
 using System.Net;
-using System.IO;
 using System.Threading;
 
 namespace _24_8_17_Server
